ff61e173e2c0f963b57927ec0aa783f6afaac3eb
Kasyanov-Igor <igor.kasyanov00@mail.ru>
2025-06-24 21:33:10 +0300

    Added connecting to the database and creating models. Closed #2

---
+++ Model/Entities/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Test_task.Model
+{
+	public class Employee
+	{
+		public int Id { get; set; }
+		public string UserName { get; set; }
+		public string JobTitle { get; set; }
+		public DateTime DOB { get; set; }
+	}
+}
---
+++ Model/Entities/Order.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Test_task.Model.Entities
+{
+	public class Order
+	{
+		public int Id { get; set; }
+		public DateTime dateTime { get; set; }
+		public uint Amount { get; set; }
+		public int EmployeeId { get; set; }
+		public int OrderId { get; set; }
+	}
+}
