f32dd6dac62c240fee1ffc09f908592e70dcc6a6
Igor <161635179+Kasyanov-Igor@users.noreply.github.com>
2025-06-25 22:19:40 +0300

    Addedd MySQL connection. Closed #12

---
+++ TestTask/Services/MySQLConnection.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Test_task.Services.Interfaces;
+
+namespace TestTask.Services
+{
+    public class MySQLConnection : ADatabaseConnection
+    {
+        public const string _DATABASE_NAME = "../DB.db";
+
+        public const string USER = "user";
+
+        public const string PASSWORD = "12345678";
+
+        protected override string ReturnConnectionString()
+        {
+            return $"server=localhost;user={USER};password={PASSWORD};database={_DATABASE_NAME}";
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(this.ConnectionString, new MySqlServerVersion(new Version(8, 0, 40)));
+        }
+    }
+}
