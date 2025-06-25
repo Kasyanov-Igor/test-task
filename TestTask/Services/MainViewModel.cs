56dff28c323124e24caab369433f52916436b614
Igor <161635179+Kasyanov-Igor@users.noreply.github.com>
2025-06-25 18:34:16 +0300

    Added implementation of adding Counterparty entity in a separate widget. Closed #8

---
+++ TestTask/Services/MainViewModel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Test_task.Services.Interfaces;
+
+namespace TestTask.Services
+{
+	public class MainViewModel : ViewModel
+	{
+		public CounterpartyViewModel CounterpartyVM { get; }
+		public OrderViewModel OrderVM { get; }
+		public EmployeeViewModel EmployeeVM { get; }
+
+		private ViewModel _currentViewModel;
+		public ViewModel CurrentViewModel
+		{
+			get => _currentViewModel;
+			set => SetProperty(ref _currentViewModel, value);
+		}
+
+		public ObservableCollection<string> ModelNames { get; } = new ObservableCollection<string>
+		{
+		"Counterparty",
+		"Order",
+		"Employee"
+		};
+
+		private string _selectedModelName;
+		public string SelectedModelName
+		{
+			get => _selectedModelName;
+			set
+			{
+				if (SetProperty(ref _selectedModelName, value))
+				{
+					switch (value)
+					{
+						case "Counterparty":
+							CurrentViewModel = CounterpartyVM;
+							break;
+						case "Order":
+							CurrentViewModel = OrderVM;
+							break;
+						case "Employee":
+							CurrentViewModel = EmployeeVM;
+							break;
+					}
+				}
+			}
+		}
+
+		public MainViewModel(
+			CounterpartyViewModel counterpartyVM,
+			OrderViewModel orderVM,
+			EmployeeViewModel employeeVM)
+		{
+			CounterpartyVM = counterpartyVM;
+			OrderVM = orderVM;
+			EmployeeVM = employeeVM;
+
+			SelectedModelName = ModelNames.First();
+		}
+	}
+}
---
+++ TestTask/Services/OrderViewModel.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Test_task.Model.Entities;
+using Test_task.Repositories.Interface;
+using Test_task.Services.Interfaces;
+
+namespace TestTask.Services
+{
+    public class OrderViewModel : ViewModel
+    {
+        private readonly IRepository<Order> _repository;
+
+        public ObservableCollection<Order> Orders { get; } = new();
+
+        private Order _selectedOrder;
+        public Order SelectedOrder
+        {
+            get => _selectedOrder;
+            set => SetProperty(ref _selectedOrder, value);
+        }
+
+        public ICommand LoadCommand { get; }
+        public ICommand AddCommand { get; }
+        public ICommand DeleteCommand { get; }
+
+        public OrderViewModel(IRepository<Order> repository)
+        {
+            _repository = repository;
+
+            LoadCommand = new RelayCommand(async _ => await LoadAsync());
+            AddCommand = new RelayCommand(async _ => await AddAsync());
+            DeleteCommand = new RelayCommand(async _ => await DeleteAsync());
+        }
+
+        private async Task LoadAsync()
+        {
+            var items = await _repository.Get();
+
+            Orders.Clear();
+            foreach (var item in items)
+                Orders.Add(item);
+        }
+
+        private async Task AddAsync()
+        {
+            var newOrder = new Order
+            {
+                dateTime = DateTime.Now,
+                Amount = 0,
+                EmployeeId = 0,
+                OrderId = 0
+            };
+            await _repository.Add(newOrder);
+            Orders.Add(newOrder);
+        }
+
+        private async Task DeleteAsync()
+        {
+            if (SelectedOrder == null) return;
+
+            await _repository.Delete(SelectedOrder.Id);
+            Orders.Remove(SelectedOrder);
+        }
+    }
+}
